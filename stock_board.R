6c4543c7580e7719c8b67154fb6596d5837f9490
TimChen <timchen@kkbox.com>
2017-06-10 14:41:24 +0800

    Add record function to filter non-integer value

--- stock_board.R
+++ stock_board.R
@@ -23,13 +23,37 @@ host <- sub(url, pattern = ".html", replacement = "")
 
 #最有貢獻王
 
+#Refer to http://pttpedia.pixnet.net/blog/post/40163758-噓文（噓爆）
+toGetRecord <- function(record) {
+  if(record == "爆") {
+    #Fix me : https://github.com/fatfattim/ptt_mining/issues/1
+    record <- 100
+  } else if(grepl("XX", record)) {
+    record <- -100
+  } else if(grepl("X", record)) {
+    record <- as.integer(gsub("X", "", record)) * (-10)
+  }
+  return (record)
+}
+
+# ----- Unit test for toGetRecord -----
+X <- toGetHateRecord("XX")
+stopifnot(X == -100)
+X <- toGetRecord("X1")
+stopifnot(X == -10)
+X <- toGetRecord("X5")
+stopifnot(X == -50)
+X <- toGetRecord("爆")
+stopifnot(X == 100)
+# ----- Unit test for toGetRecord -----
+
 toGetFamousResult <- function() {
   
   FamousFrame <- data.frame(user= character(0), record = character(0), stringsAsFactors=FALSE)
-  FamousFrame[1, ] <- c("", as.integer(123))
+  FamousFrame[1, ] <- c(NA, NA)
   
-  for( i in (lastPage - 20):lastPage) {
-    tempUrl <- paste0(host, i, ".html")
+  for( i in (lastPage):lastPage) {
+    tempUrl <- paste0(host, 3510, ".html")
     html <- htmlParse(getURL(tempUrl, cainfo = signatures))
     tempUrl.list <- unlist(xpathSApply(html, "//div[@class='r-ent']", xmlNode))
     tempUrl.list[which(names(tempUrl.list) %in% c("namespace"))] <- NULL
@@ -43,11 +67,14 @@ toGetFamousResult <- function() {
     
     for (i in 1:nrow(data)) {
       row <- data[i,]
+
+      record <- toGetRecord(row$record)
+      
       index <- FamousFrame$user %in% row$user
       if(any(index>0)) {
         FamousFrame[index ,]$record = as.integer(row$record) + as.integer(FamousFrame[index ,]$record)
       } else {
-        FamousFrame[nrow(FamousFrame) + 1 , ] <- c(row$user , as.integer(row$record))
+        FamousFrame[nrow(FamousFrame) + 1 , ] <- c(row$user , as.integer(record))
       }
     }
   }
@@ -55,6 +82,10 @@ toGetFamousResult <- function() {
   return (FamousFrame)
 }
 
+result <- toGetFamousResult()
+
+#To Do
+#非戰之罪 : cheinshin    □ [新聞] 兆豐銀遭罰57億 曾銘宗：人事空窗期沒人
 
 
 
